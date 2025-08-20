883dcaf142d7ad35164d253831aa6dbb291d9ac2
bee bees <ahmedyoussefabbes@gmail.com>
2025-08-20 16:02:06 +0000

    le probleme persiste analyse profondementet trouve la solution

--- src/components/classes/ClassesView.tsx
+++ src/components/classes/ClassesView.tsx
@@ -118,7 +118,7 @@ const ClassesView: React.FC<ClassesViewProps> = ({ grades, classes, userRole, in
         
         {/* DETAIL CARDS (Conditional Rendering) */}
         {selectedGrade && (
-            <div className="grid grid-cols-1 md:grid-cols-3 gap-6 animate-in fade-in-50 duration-500 mt-12">
+            <div className="grid grid-cols-1 md:grid-cols-3 gap-6 animate-in fade-in-50 duration-500 mt-6">
                 <DetailCard 
                     title="Classes" 
                     count={selectedGrade._count.classes} 
