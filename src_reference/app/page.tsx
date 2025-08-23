717595266604bd5916c349a692dc0857be049129
bee bees <ahmedyoussefabbes@gmail.com>
2025-08-23 19:07:08 +0000

    nouveau

--- src_reference/app/page.tsx
+++
@@ -1,8 +0,0 @@
-// src/app/page.tsx
-import { redirect } from 'next/navigation';
- 
-export default async function RootPage() {
-  // Always redirect to the main welcome/login page.
-  // The middleware will handle auth-based routing from there.
-  return redirect('/accueil');
-}
