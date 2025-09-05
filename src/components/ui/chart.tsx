e21ae9623b179692ad66a7929090c04a52b47e71
bee bees <ahmedyoussefabbes@gmail.com>
2025-09-05 20:07:39 +0000

    [{ 	"resource": "/home/user/studio/src/components/ui/chart.tsx", 	"owner

--- src/components/ui/chart.tsx
+++ src/components/ui/chart.tsx
@@ -4,6 +4,10 @@ import * as React from "react"
 import * as RechartsPrimitive from "recharts"
 
 import { cn } from "@/lib/utils"
+import {
+  NameType,
+  ValueType,
+} from "recharts/types/component/DefaultTooltipContent"
 
 // Format: { THEME_NAME: CSS_SELECTOR }
 const THEMES = { light: "", dark: ".dark" } as const
@@ -185,7 +189,7 @@ const ChartTooltipContent = React.forwardRef<
       >
         {!nestLabel ? tooltipLabel : null}
         <div className="grid gap-1.5">
-          {payload.map((item: any, index: number) => {
+          {payload.map((item, index) => {
             const key = `${nameKey || item.name || item.dataKey || "value"}`
             const itemConfig = getPayloadConfigFromPayload(config, item, key)
             const indicatorColor = color || item.payload.fill || item.color
