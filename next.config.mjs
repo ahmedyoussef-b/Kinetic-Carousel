a7a52c2bb7438ca3baa9afdd3de9214e598014cc
bee bees <ahmedyoussefabbes@gmail.com>
2025-09-06 00:56:29 +0000

    Download the React DevTools for a better development experience: https:/

--- next.config.mjs
+++ next.config.mjs
@@ -1,23 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-    srcDir: true,
-  },
   output: 'standalone',
-  async headers() {
-    return [
-      {
-        source: '/:path*',
-        headers: [
-          {
-            key: 'X-Frame-Options',
-            value: 'ALLOW-FROM *',
-          },
-        ],
-      },
-    ];
-  },
 };
 
 export default nextConfig;
